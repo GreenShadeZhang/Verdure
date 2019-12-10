1215e366dc1154314f0f9fee09f38d8d8f033e65
zhangguangjian <zhangguangjian@huodongxing.com>
2019-12-10 10:54:55 +0800

    添加修改数据库表名

--- GreenShade.DataAccess/Data/AppIdentityDbContext.cs
+++ GreenShade.DataAccess/Data/AppIdentityDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GreenShade.Blog.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,6 +13,18 @@ namespace GreenShade.Blog.DataAccess.Data
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Add your customizations after calling base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
         }
     }
 }
