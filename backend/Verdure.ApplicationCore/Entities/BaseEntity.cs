6728edcfd80873e5db6223b60bbd6093329b6d76
GreenShadeZhang <gil.zhang.dev@outlook.com>
2021-03-23 21:26:01 +0800

    添加一些类

---
+++ backend/Verdure.ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verdure.ApplicationCore.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+    }
+}
---
+++ backend/Verdure.ApplicationCore/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verdure.ApplicationCore.Interfaces
+{
+    public interface IAggregateRoot
+    { }
+}
