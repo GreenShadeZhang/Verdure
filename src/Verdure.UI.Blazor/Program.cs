54754ef255a15a073e7fc832dec981839364b6fa
Gil Zhang <gil.zhang.dev@outlook.com>
2021-12-05 00:27:30 +0800

    调整项目的位置

--- src/Verdure.UI.Blazor/Program.cs
+++ src/Verdure.UI.Blazor/Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Verdure.UI.Blazor;
+using MudBlazor.Services;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7010") });
+builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
