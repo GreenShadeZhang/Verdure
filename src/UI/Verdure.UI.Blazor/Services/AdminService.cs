076a6ffcd6c0ee0de902257ac47372b73ced249a
gil zhang <gil.zhang.dev@outlook.com>
2021-12-25 22:06:48 +0800

    更新配置

--- src/UI/Verdure.UI.Blazor/Services/AdminService.cs
+++ src/UI/Verdure.UI.Blazor/Services/AdminService.cs
@@ -5,13 +5,17 @@ namespace Verdure.UI.Blazor
     public class AdminService
     {
         private readonly HttpClient _httpClient;
-        public AdminService(HttpClient httpClient)
+        private readonly IConfiguration _configuration;
+        public AdminService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
         public async Task<bool> AddAsync(HttpContent? content, CancellationToken cancellationToken = default)
         {
-            var ret = await _httpClient.PostAsync("/api/Admin/ImportArticle", content);
+            var url = $"{_configuration["AdminPrefix"]}/api/Admin/ImportArticle";
+
+            var ret = await _httpClient.PostAsync(url, content);
 
             if (ret != null && ret.StatusCode == HttpStatusCode.OK)
             {
--- src/UI/Verdure.UI.Blazor/Services/ArticleService.cs
+++ src/UI/Verdure.UI.Blazor/Services/ArticleService.cs
@@ -6,13 +6,17 @@ namespace Verdure.UI.Blazor
     public class ArticleService
     {
         private readonly HttpClient _httpClient;
-        public ArticleService(HttpClient httpClient)
+        private readonly IConfiguration _configuration;
+        public ArticleService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
         public async Task<List<Article>?> GetListAsync(QueryRequest request, CancellationToken cancellationToken = default)
         {
-            return await _httpClient.GetFromJsonAsync<List<Article>>("/api/Articles/GetList?PageIndex=1&PageSize=100");
+            var url = $"{_configuration["AdminPrefix"]}/api/Articles/GetList?PageIndex=1&PageSize=100";
+
+            return await _httpClient.GetFromJsonAsync<List<Article>>(url);
         }
     }
 }
